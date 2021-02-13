801e7c704e6abbbe329063ea0eaa1e99d9563d42
nazeemnato <nazeemnob18@gmail.com>
2021-02-13 19:53:43 +0530

    learning broo

---
+++ server.js
@@ -0,0 +1,13 @@
+const { ApolloServer } = require('apollo-server');
+
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
